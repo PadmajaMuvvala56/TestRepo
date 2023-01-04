a6aba8c5c06754a518f0bfd866bbd3f161029e18
ajayk <ajayk@PADMAJAGAJAVALLI>
2023-01-04 11:52:25 +0530

    ExcelUtility project commit

---
+++ ExcelUtility/src/test/java/ExcelUtils/ExcelUtilCls.java
@@ -0,0 +1,34 @@
+package ExcelUtils;
+
+import java.io.IOException;
+
+import org.apache.poi.ss.usermodel.DataFormatter;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class ExcelUtilCls {
+	static XSSFWorkbook workbook ;
+	static XSSFSheet sheet ;
+	public ExcelUtilCls(String excelPath,String sheetName) throws IOException
+	{
+		  workbook = new XSSFWorkbook(excelPath);
+		  sheet = workbook.getSheet(sheetName);
+	}
+
+	public void getCellData(int rowNum, int colNum) 
+	{
+		
+		DataFormatter formatter = new DataFormatter();
+		Object value = 	formatter.formatCellValue(sheet.getRow(rowNum).getCell(colNum));
+		System.out.println(value);
+	}
+	public void getRowCount() 
+	{
+		int rowCount = sheet.getPhysicalNumberOfRows();
+		System.out.println("Rows count:" + rowCount);
+	}
+
+
+
+
+}
---
+++ ExcelUtility/src/test/java/ExcelUtils/ExcelUtilsTestMain.java
@@ -0,0 +1,25 @@
+package ExcelUtils;
+
+public class ExcelUtilsTestMain {
+
+	public static void main(String[] args) 
+	{
+		try 
+		{
+		String excelPath = "./data/TestData.xlsx";
+		String sheetName = "Sheet1";	
+		ExcelUtilCls excel = new ExcelUtilCls(excelPath, sheetName);
+		excel.getRowCount();
+		excel.getCellData(1,0);
+		excel.getCellData(1,1);
+		excel.getCellData(1,2);
+		}
+		catch(Exception exp)
+		{
+		System.out.println(exp.getMessage());
+		System.out.println(exp.getCause());
+		exp.printStackTrace();
+		}
+	}
+
+}
